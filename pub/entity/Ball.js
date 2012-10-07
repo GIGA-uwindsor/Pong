f5230c53eecc3074fa65bbb2eaebf601fb8b1989
Adam Heinermann <heiner@live.ca>
2012-10-06 20:10:06 -0400

    Regulated the ball's speed.
    Regulated the ball's speed using delta time.
    Instead of checking if the ball is not moving, check if it's too slow,
    and then speed it up.
    Add setVelocityMultiplier.

--- pub/entity/Ball.js
+++ pub/entity/Ball.js
@@ -1,12 +1,42 @@
 function Ball(x,y) {
   this._GFW_Entity_Initialize();
   this.setPosition(x,y);
-  this.setVelocity(3,3);
+  this.setVelocity(.5,1);
   this.setBounds(640,480);
   this.setRadius(10);
 }
 
 Ball.prototype = {
+/* FUNCTIONALITY */
+  // Tells the ball that it was hit by a paddle.
+  // paddle:
+  //    0: hit left paddle, move ball right
+  //    1: hit right paddle, move ball left
+  // direction:
+  //   -1: paddle was moving up
+  //    0: paddle was not moving
+  //    1: paddle was moving down
+  hitPaddle: function (paddle,direction) {
+    // @TODO
+    // - Invert the X velocity
+    // - Change the Y velocity based on the direction the paddle was moving.
+  },
+  
+  // Tells the ball to move either left or right at the given multiple of its current velocity.
+  //
+  // multiplier: The multiplier to apply to the ball's current X velocity.
+  //
+  // absolute: Boolean specifying the behaviour of this function.
+  //      true = Multiplier is absolute. A negative multiplier moves the ball left,
+  //              and a positive multiplier moves it right.
+  //      false = Multiplier is relative. A negative multiplier moves the ball in the
+  //              opposite direction it is currently moving.
+  setVelocityMultiplier: function(multiplier, absolute) {
+    var vx = absolute ? Math.abs(this.getVX()) : this.getVX();
+    this.setVX(vx * multiplier);    
+  },
+  
+/* CONVENIENCE */
   // Sets the position of the Ball
   setPosition: function (x,y) {
     this.setX(x);
@@ -23,6 +53,8 @@ Ball.prototype = {
     this.setBoundY(y);
   },
 
+/* CALLBACKS */  
+  // UPDATE
   update: function (updateParams) {
     // Localize variables
     var x = this.getX();
@@ -38,6 +70,13 @@ Ball.prototype = {
     var top     = y - r;
     var right   = x + r;
     var bottom  = y + r;
+    
+    // Get the delta time since last update
+    var delta = updateParams.getTime().getDelta();
+    if ( Math.abs(delta) > 10000 )
+      return;
+    
+    //console.log("delta: %d", delta);
 
     // Bounce off of the walls
     if ( left <= 0 || right >= boundX ) // left/right
@@ -47,14 +86,25 @@ Ball.prototype = {
       this.setVY( (top <= 0 ? 1 : -1) * Math.abs(this.getVY()) );
 
     // Update the ball's position
-    this.setPosition( x + this.getVX(), y + this.getVY() );
+    this.setPosition( x + this.getVX()*delta/10, y + this.getVY()*delta/10 );
     
-    // Randomize the ball's X velocity if there is none
-    if ( this.getVX() == 0 )
-      this.setVX( (Math.random() < 0.5 ? 1 : -1) * // either -1 or 1, multiplied by...
-                    Math.floor(Math.random()*3 + 1) );  // a number from 1 to 3?
+    // Randomize the ball's X velocity if it's either not moving, or too slow
+    if ( Math.abs(this.getVX()) < 0.5 )
+    {
+      var direction;
+      if ( this.getVX() < 0 )
+        direction = -1;
+      else if ( this.getVX() > 0 )
+        direction = 1;
+      else // VX is 0
+        direction = (Math.random() < 0.5 ? 1 : -1);
+      
+      this.setVX( direction * // either -1 or 1, multiplied by...
+                  Math.random()+1 );  // a random number from 1 to 2
+    }
   },
 
+  // DRAW
   draw: function (ctx, assets) {
     // Project the ball onto the drawing canvas
     var x = (this.getX() / this.getBoundX()) * ctx.canvas.width;
