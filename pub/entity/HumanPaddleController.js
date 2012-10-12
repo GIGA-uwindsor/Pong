9f8afa0bedc12fb847476ec20e622156e571853d
Eric Brisco <eric.brisco@gmail.com>
2012-10-12 14:56:31 -0400

    Created HumanPaddleController. Resolves issue 9

---
+++ pub/entity/HumanPaddleController.js
@@ -0,0 +1,28 @@
+function HumanPaddleController(paddle) {
+  this.set__paddle(paddle);
+}
+
+HumanPaddleController.prototype = {
+  
+  update: function (updateParams) {
+    var keyboard = updateParams.getKeyboard();
+    var goUp = keyboard.isDown(GFW_Key.w) || keyboard.isDown(GFW_Key.upArrow);
+    var goDown = keyboard.isDown(GFW_Key.s) || keyboard.isDown(GFW_Key.downArrow);
+    
+    if ((!goUp && goDown) || (goUp && !goDown))
+      if (goUp) {
+        this.get__paddle().setVelocY(-1);
+      }
+      else {
+        this.get__paddle().setVelocY(1);
+      }
+    }
+    else {
+      this.get__paddle().setVelocY(0);
+    }
+  }
+  
+}
+
+GFW_Property(HumanPaddleController, "__paddle");
+GFW_mixin(HumanPaddleController, GFW_Entity, ["update"]);
